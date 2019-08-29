076927157ff0846e5ada5d49646fedfe17b4d852
monkeycoderw <3897177+monkeycoderw@users.noreply.github.com>
2019-08-29 23:00:33 +0800

    反射策略模式

--- src/main/java/com/nick/design/mode/demo/builder/BuilderClient.java
+++ src/main/java/com/nick/design/mode/demo/builder/BuilderClient.java
@@ -8,7 +8,7 @@ package com.nick.design.mode.demo.builder;
  */
 public class BuilderClient {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ClassNotFoundException,InstantiationException, IllegalAccessException{
     ComputerDirector director = new ComputerDirector();
 
     Builder dellBuilder = new DellComputerBuilder();
@@ -18,6 +18,12 @@ public class BuilderClient {
     Builder aussBuilder = new ASUSComputerBuilder();
     Computer auss = director.construct(aussBuilder);
     System.out.println(auss.toString());
+
+
+    Class c = Class.forName("com.nick.design.mode.demo.builder.DellComputerBuilder");
+    Builder builder = (Builder) c.newInstance();
+    Computer computer = director.construct(builder);
+    System.out.println(computer.toString());
   }
 
 }
